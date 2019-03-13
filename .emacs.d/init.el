20aaecbe7bfecd0796efd81125f329e16305c746
YukihiroMasuoka <yukihiro3socrates6hilbert@gmail.com>
2019-03-13 23:58:30 +0900

    Change directory to use powershell as shell.
    I define a function to change load-path.
    Then, I add Mr.Dino Chiesa's powershell.el.

---
+++ .emacs.d/init.el
@@ -0,0 +1,15 @@
+;;Define the function to add load-path.
+(defun add-to-load-path (&rest paths)
+  (let (path)
+    (dolist (path paths paths)
+      (let ((default-directory
+	      (expand-file-name (concat user-emacs-directory path))))
+	(add-to-list 'load-path default-directory)
+	(if (fboundp 'normal-top-level-add-subdirs-to-load-path)
+	    (normal-top-level-add-subdirs-to-load-path))))))
+;; Add ~\.emacs.d\elisp to load-path.
+(add-to-load-path "elisp")
+
+;; To use Powershell.
+(autoload 'powershell "powershell.el" "Run powershell as a shell within emacs." t)
+
