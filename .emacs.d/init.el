7e63142a62d890495c8e4a86af880f2a596ad2ae
YukihiroMasuoka <yukihiro3socrates6hilbert@gmail.com>
2019-03-21 01:15:05 +0900

    Merge init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -7,6 +7,7 @@
 	(add-to-list 'load-path default-directory)
 	(if (fboundp 'normal-top-level-add-subdirs-to-load-path)
 	    (normal-top-level-add-subdirs-to-load-path))))))
+
 ;; Add ~\.emacs.d\elisp to load-path.
 (add-to-load-path "elisp")
 
@@ -14,3 +15,15 @@
 (when (or (eq system-type 'windows-nt) (eq system-type 'cygwin))
  (autoload 'powershell "powershell.el" "Run powershell as a shell within emacs." t))
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(inhibit-startup-screen t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
