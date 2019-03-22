879ee81435f0f2be01b65dc2c360645822466e57
YukihiroMasuoka <yukihiro3socrates6hilbert@gmail.com>
2019-03-22 19:18:21 +0900

    Delete ddskk.

--- .emacs.d/elpa/ccc-20151205.1343/ccc-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "ccc" "20151205.1343" "buffer local cursor color control library" 'nil :commit "1e73b77d7dc9cd812d90bdfce10378928f57508a" :keywords '("cursor") :authors '(("Masatake YAMATO" . "masata-y@is.aist-nara.ac.jp")) :maintainer '("SKK Development Team" . "skk@ring.gr.jp") :url "https://github.com/skk-dev/ddskk/blob/master/READMEs/README.ccc.org")
