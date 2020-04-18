60a45cd5a0d66c46496c6e0e3dbab9f57a962623
YukihiroMasuoka <yukihiro3socrates6hilbert@gmail.com>
2020-04-19 00:07:50 +0900

    Add .gitignore

--- .emacs.d/elpa/dash-20190320.1406/dash-autoloads.el
+++
@@ -1,17 +0,0 @@
-;;; dash-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil nil ("../../../../../.emacs.d/elpa/dash-20190320.1406/dash-autoloads.el"
-;;;;;;  "../../../../../.emacs.d/elpa/dash-20190320.1406/dash.el")
-;;;;;;  (23705 59081 741970 100000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; dash-autoloads.el ends here
--- .emacs.d/elpa/transient-20190319.1928/transient-autoloads.el
+++
@@ -1,18 +0,0 @@
-;;; transient-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil nil ("../../../../../.emacs.d/elpa/transient-20190319.1928/transient-autoloads.el"
-;;;;;;  "../../../../../.emacs.d/elpa/transient-20190319.1928/transient-pkg.el"
-;;;;;;  "../../../../../.emacs.d/elpa/transient-20190319.1928/transient.el")
-;;;;;;  (23705 59082 641964 584000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; transient-autoloads.el ends here
