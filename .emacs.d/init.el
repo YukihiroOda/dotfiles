ccebf28d3b0e57e72d975495e9b46647d616e563
YukihiroMasuoka <yukihiro3socrates6hilbert@gmail.com>
2020-05-13 18:18:17 +0900

    Change eww-serch-prefix +-sejuku etc.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -50,7 +50,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(eww-search-prefix "https://www.google.co.jp/search?q=")
+ '(eww-search-prefix "https://www.google.com/search?q=-sejuku+-wa3.i-3-i.info+")
  '(inhibit-startup-screen t)
  '(js-indent-level 2)
  '(make-backup-files nil)
